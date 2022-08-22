8456eea842505a0ae9c1ae90b5807b745105791d
duyguadadivle <duyguadadivle@gmail.com>
2022-08-22 12:29:09 +0300

    routing.module

--- e-commerceApp-frontend/src/app/app-routing.module.ts
+++ e-commerceApp-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './component/products/product/product.component';
+import { ProductsComponent } from './component/products/products.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: ProductsComponent },
+  { path: 'detail/:id', component: ProductComponent},
+  { path: 'category/:name', component: ProductsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'communication', component: ProductsComponent },
+  { path: 'basket', component: ProductsComponent },
+  { path: 'shoppingCart', component: ShoppingcartComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CommonModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
--- e-commerceApp-frontend/src/app/app.module.ts
+++ e-commerceApp-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './component/products/products.component';
+import { ProductComponent } from './component/products/product/product.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductComponent,
+    ShoppingcartComponent
   ],
   imports: [
     BrowserModule,
