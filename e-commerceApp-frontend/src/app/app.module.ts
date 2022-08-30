4c2c61e349adae612c02537216053b96f2c0cb1a
duyguadadivle <duyguadadivle@gmail.com>
2022-08-30 12:08:27 +0300

    app.modele.ts

--- e-commerceApp-frontend/src/app/app.module.ts
+++ e-commerceApp-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,13 +13,15 @@ import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
     AppComponent,
     ProductsComponent,
     ProductComponent,
-    ShoppingcartComponent
+    ShoppingcartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
