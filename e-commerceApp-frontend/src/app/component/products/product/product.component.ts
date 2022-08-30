d4205a8f5d544afe84d51b3e85dd706e76d30772
duyguadadivle <duyguadadivle@gmail.com>
2022-08-30 12:09:19 +0300

    product componrnt

--- e-commerceApp-frontend/src/app/component/products/product/product.component.ts
+++ e-commerceApp-frontend/src/app/component/products/product/product.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../model/product';
+import { Location } from '@angular/common';
+
+//import { Products } from '../../../mock/mock-products';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +12,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  //products: Product[] = [];
 
-  constructor() { }
+  //selectedProducts?: Product;
 
+   product: Product | undefined;
+   
+   constructor(
+     private route: ActivatedRoute,
+     private productService: ProductService,
+     private location: Location
+   ) {} 
+
+  getProduct(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getProduct(id)
+      .subscribe(product => this.product = product)  }
+
+  /*onSelect(product: Product): void {
+    this.selectedProducts = product;
+  }
+  showLoader(){
+
+  }*/
   ngOnInit(): void {
+    this.getProduct();
+  }
+  addToShoppingCart(product: any){
+    console.log(product);
+    this.productService.addProduct(product);
   }
 
+  
 }
