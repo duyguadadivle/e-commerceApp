562b1cc42b2d56c13f76535e060cbd7fa76c2a75
duyguadadivle <duyguadadivle@gmail.com>
2022-09-01 14:56:29 +0300

    export db

---
+++ e-commerceApp-backend/src/main/java/com/example/ecommerceappbackend/db/product.sql
@@ -0,0 +1,27 @@
+"id","category_name","description","image_path","name","price"
+1153,"elektronik","Monster Abra A5 V18.1.1","https://cdn.dsmcdn.com/ty343/product/media/images/20220228/15/59976124/191277609/1/1_org_thumb.jpg","Monster Abra A5 V18.1.1",2200
+1154,"erkek","Avva Erkek Beyaz Polo Yaka Düz T-shirt","https://cdn.dsmcdn.com//ty453/product/media/images/20220613/9/124980413/66147235/1/1_org.jpg","AVVA Erkek T-shirt",119
+1155,"elektronik","Apple iPhone 12 64GB Yeşil Cep Telefonu","https://cdn.dsmcdn.com/ty198/product/media/images/20211014/17/147832282/113336027/1/1_org_thumb.jpg","Iphone 12",1300
+1156,"cocuk","Entel Tasarım  Kız Çocuk Çilek Elbise ","https://cdn.dsmcdn.com/ty385/product/media/images/20220404/23/83148986/436840594/1/1_org_thumb.jpg","Kız Çocuk Çilekli Elbise",89
+1157,"ev%20ve%20yasam"," Minderli - Nora Şezlong Seti ","https://cdn.dsmcdn.com/ty99/product/media/images/20210408/16/78743036/161957054/1/1_org_thumb.jpg","Minderli Şezlong Seti",399
+1158,"supermarket","PERFECT Kuzu Etli Yetişkin Köpek Maması ","https://cdn.dsmcdn.com/ty331/product/media/images/20220214/10/49986274/158102304/1/1_org_thumb.jpg","Yavru Köpek Maması",220
+1159,"kadin","Mango Düz Kesimli Yüksek Bel Jean Pantolon","https://cdn.dsmcdn.com/ty335/product/media/images/20220217/20/52082329/321435088/1/1_org_thumb.jpg","Mango Yüksek Bel Jean",150
+1160,"ev ve yasam","Antre Avize","https://cdn.dsmcdn.com/mnresize/1200/1800/ty429/product/media/images/20220513/13/110426189/437438329/1/1_org_zoom.jpg","Üçlü Avize",220
+1161,"cocuk","Haki Erkek Çocuk Dokuma Keten Şort","https://cdn.dsmcdn.com/mnresize/1200/1800/ty458/product/media/images/20220621/14/128445522/441248479/3/3_org_zoom.jpg","Çocuk Şort",330
+1162,"elektronik","HyperX Cloud II Oyuncu Kulaklık","https://cdn.dsmcdn.com/ty432/product/media/images/20220516/13/112117585/13870166/2/2_org_thumb.jpg","HyperX Cloud II",200
+1163,"kadin","Mongo Kadın Lila Oversize T-shirt","https://cdn.dsmcdn.com/ty400/product/media/images/20220416/15/92960861/450766723/1/1_org_thumb.jpg","Oversize T-shirt",50
+1164,"elektronik","Sony Playstation 5 + 2. PS5 DualSense","https://cdn.dsmcdn.com/ty32/product/media/images/20210326/23/75554837/108939192/1/1_org_thumb.jpg","Playstation 5 ",1700
+1165,"kadin","MOONBULL Kadın Siyah Kapüşonlu Sweatshirt","https://cdn.dsmcdn.com/ty270/product/media/images/20211214/22/11117603/274322303/1/1_org_thumb.jpg","Kadın Sweatshirt",90
+1166,"kadin","Yingyang Baskılı Tshirt TSH-yingyang","https://cdn.dsmcdn.com/ty447/product/media/images/20220607/15/122564243/406881016/1/1_org_thumb.jpg","Kadın T-shirt",80
+1167,"elektronik","Apple Macbook Pro 13 M1 Ssd Uzay Grisi","https://cdn.dsmcdn.com/ty208/product/media/images/20211025/14/155922944/124608528/1/1_org_thumb.jpg","Macbook Pro M1",2000
+1168,"elektronik","Samsung 27 Freesync VA Gamıng Monıtor","https://cdn.dsmcdn.com/ty265/product/media/images/20211206/12/5488958/331013838/1/1_org_thumb.jpg","Odyssey Monitör ",2999
+1169,"aksesuar","Mathilda Unisex Güneş Gözlüğü","https://cdn.dsmcdn.com/ty157/product/media/images/20210812/11/118001523/148670287/1/1_org_thumb.jpg","Unisex Güneş Gözlüğü ",299
+1170,"erkek","Örme Oversize Beyaz T-shirt","https://cdn.dsmcdn.com/ty395/product/media/images/20220413/0/89633830/162251546/1/1_org_zoom.jpg","Beyaz T-shirt",199
+1171,"cocuk","GAP Erkek BebekKapüşonlu Sweatshirt","https://cdn.dsmcdn.com/mnresize/1200/1800/ty503/product/media/images/20220810/12/157427876/239769720/1/1_org_zoom.jpg","Gap Çocuk T-shirt",170
+1172,"supermarket","Çakır Çerez Lüks Karışık Kuruyemiş 1 Kg","https://cdn.dsmcdn.com/mnresize/1200/1800/ty31/product/media/images/20201203/8/34646817/22254398/1/1_org_zoom.jpg","Karışık Kuruyemiş",130
+1173,"kadin","V Yaka Dökümlü Salaş Maxi Saten Elbise ","https://cdn.dsmcdn.com/mnresize/1200/1800/ty502/product/media/images/20220810/17/157580809/540795050/1/1_org_zoom.jpg","Kadın elbise",130
+1174,"kozmetik","Shower Fresh 200 ml X3 Adet","https://cdn.dsmcdn.com/mnresize/1200/1800/ty507/product/media/images/20220815/15/160183215/58342769/1/1_org_zoom.jpg","Rexona Kadın Sprey",140
+1175,"spor ve outdoor","HUMMEL Unisex Sırt Çantası - David Back Pack","https://cdn.dsmcdn.com/mnresize/1200/1800/ty86/product/media/images/20210317/15/73072539/15837335/1/1_org_zoom.jpg","Hummal Çanta",399
+1176,"erkek","Polo Yaka Cepli Regular Fit Bol Kesim Düz Tişört","https://cdn.dsmcdn.com/mnresize/1200/1800/ty138/product/media/images/20210630/14/105799921/150342417/1/1_org_zoom.jpg","Lacivert T-shirt",120
+1177,"supermarket","Selection Filtre Kahve 250 gr 4 Al 3 Öde","https://cdn.dsmcdn.com/mnresize/1200/1800/ty35/product/media/images/20201218/8/39547279/66906671/1/1_org_zoom.jpg","Jacobs",150
+1178,"cocuk","Kız Çocuk Şort Takım","https://cdn.dsmcdn.com/mnresize/1200/1800/ty434/product/media/images/20220518/4/112958623/481728584/1/1_org_zoom.jpg","Çocuk Takım",119
