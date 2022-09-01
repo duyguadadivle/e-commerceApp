4a04c19977f176a19fb25d4e7ab48818041a05f7
duyguadadivle <duyguadadivle@gmail.com>
2022-09-01 10:59:12 +0300

    fixed

--- e-commerceApp-backend/src/main/java/com/example/ecommerceappbackend/ECommerceAppBackendApplication.java
+++ e-commerceApp-backend/src/main/java/com/example/ecommerceappbackend/ECommerceAppBackendApplication.java
@@ -10,7 +10,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 
-//@EnableSwagger2
 @SpringBootApplication
 public class ECommerceAppBackendApplication {
 
