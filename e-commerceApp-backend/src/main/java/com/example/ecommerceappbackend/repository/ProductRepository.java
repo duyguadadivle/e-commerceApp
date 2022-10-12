6b7ca01f15fa1f807ace74a0b1d344a26577a638
duyguadadivle <duyguadadivle@gmail.com>
2022-10-12 16:51:27 +0300

    updated

--- e-commerceApp-backend/src/main/java/com/example/ecommerceappbackend/repository/ProductRepository.java
+++ e-commerceApp-backend/src/main/java/com/example/ecommerceappbackend/repository/ProductRepository.java
@@ -9,8 +9,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-   //List<Product> findByName( String name);
-   //Optional<Product> findById( Long id);
 
    List<Product> findByCategoryName( String CategoryName);
 
--- e-commerceApp-frontend/src/app/mock/mock-products.ts
+++
@@ -1,6 +0,0 @@
-/*import {Product} from '../model/product';
-
-export const Products: Product[] = [
-  
-];
-*/
